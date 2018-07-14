ef7528b65f31409f9b2f7db706557042cbda8527
chuangj6 <chuangj6@uci.edu>
2018-07-13 22:30:15 -0700

    xx

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express')
+const express = require('express');
+var app = express();
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require('http').Server(app);
+const io = require('socket.io')(http);()
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('index'))
+  http.listen(PORT, function() {console.log(`Listening on ${ PORT }`);});
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', function (req,res){
+    res.sendFile(__dirname + '/about.ejs');
+});
+/*
+app.get ('/about',function(req,res){
+  res.sendFile(__dirname+'/about.ejs');
+});*/
+  io.on('connection', function(socket){
+    socket.on('playerEvent', function(msg){
+      console.log(msg);
+      io.sockets.emit('play/pause',msg);
+    }
+  );
+    console.log('a user connected:');
+    socket.on('time', function(time){
+      console.log(time);
+      io.sockets.emit('settime',time);
+    })
+
+  });
